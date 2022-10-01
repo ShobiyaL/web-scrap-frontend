2c8a12590bbb57ff0c7414d5c52be5433bb4d8ab
sofy sofya <sofy7468612@gmail.com>
2022-10-02 01:49:49 +0530

    efrtgy

--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import React ,{useState,useEffect} from 'react';
+import React  from 'react';
 import './searchbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faArrowRight} from '@fortawesome/free-solid-svg-icons';
