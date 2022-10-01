fceeb28937fa9d586f42a9d14ffd83db768125e4
sofy sofya <sofy7468612@gmail.com>
2022-10-02 01:24:18 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
-import {useState,useEffect} from 'react';
+import {useEffect} from 'react';
 // import {env} from '../../config'
 import axios from 'axios';
 import Searchbar from './components/searchbar/Searchbar';
 import Listings from './components/listings/Listings';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 function App() {
-const [products,setProducts] =useState([]);
+
 useEffect(()=>{
   loadData();
 },[]);
